5176103e23b2b0d7c5be8f44eae55132ccf0e3bc
shindeaboli8055@gmail.com <shindeaboli8055@gamil.com>
2026-01-04 13:11:40 +0530

    add ExcelReader class

---
+++ src/main/java/PageLayer/ExcelReder.java
@@ -0,0 +1,18 @@
+package PageLayer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import BaseLayer.BaseClass;
+
+public class ExcelReder extends BaseClass {
+	
+	public static void excelRed() throws FileNotFoundException
+	{
+	File f=new File("C:\\Users\\OM\\git\\BDDCogmento\\src\\test\\resources\\TestData\\TestDatamilind.xlsx");
+	FileInputStream fis=new FileInputStream(f);
+	//XSSFWorkBook xssfwork=new XSSFWorkBook(fis);
+
+	}
+}
